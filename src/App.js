5d8c888b5eb3b2744e2e14798d9e4cdadb3aec39
Valentin Ageev <sazuru@gmail.com>
2020-03-22 16:51:41 +0300

    Making a second component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
 
-function MovieItem(props) {
-  return (
-    <div>
-      <p>{props.title}</p>
-      <p>{props.vote_average}</p>
-    </div>
-  );
+const movie = {
+  title: 'Avengers: Inifnity War',
+  vote_average: 8.5,
+  image:
+    'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+  overview:
+    'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
+};
+
+function Image(props) {
+  return <img src={props.src} alt={props.alt}></img>;
+}
+
+class MovieItem extends React.Component {
+  render() {
+    const {
+      data: { title, vote_average, image },
+    } = this.props;
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+      </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div>
-      <MovieItem title="Title new" vote_average={10.1} />
+      <MovieItem data={movie} />
     </div>
   );
 }
