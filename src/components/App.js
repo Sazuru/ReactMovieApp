cb3c79d8fc48551a6cb17033456c27ea09f0174a
Valentin Ageev <sazuru@gmail.com>
2020-03-23 18:18:39 +0300

    Added delete button

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 // import { render } from '@testing-library/react';
 import { moviesData } from '../moviesData';
 
+function removeMovie(movie) {
+  const updateMovies = this.state.movies.filter(function(item) {
+    return item.id !== movie.id;
+  });
+  this.setState({
+    movies: updateMovies,
+  });
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -14,8 +23,13 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {this.state.movies.map(function(movie) {
-          return <p>{movie.title}</p>;
+        {this.state.movies.map(movie => {
+          return (
+            <div key={movie.id}>
+              <p>{movie.title}</p>
+              <button onClick={removeMovie.bind(this, movie)}>Delete</button>
+            </div>
+          );
         })}
       </div>
     );
