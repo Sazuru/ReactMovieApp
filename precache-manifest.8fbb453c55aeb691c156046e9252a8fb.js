bbcd3835ab2aa52fdb113d7a92b78c69fff39b55
Valentin Ageev <sazuru@gmail.com>
2020-03-26 10:40:04 +0300

    Updates

---
+++ precache-manifest.8fbb453c55aeb691c156046e9252a8fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d4562b3b43bd56944384da6129c804",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "0799a559ace6fdb04681",
+    "url": "/react-movie-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "65b95cd0a38e1f1030c5",
+    "url": "/react-movie-app/static/css/main.8bdb7a1c.chunk.css"
+  },
+  {
+    "revision": "0799a559ace6fdb04681",
+    "url": "/react-movie-app/static/js/2.4bc2348f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-movie-app/static/js/2.4bc2348f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b95cd0a38e1f1030c5",
+    "url": "/react-movie-app/static/js/main.b6697f28.chunk.js"
+  },
+  {
+    "revision": "98a0803a02abaab14463",
+    "url": "/react-movie-app/static/js/runtime-main.7ac0493a.js"
+  },
+  {
+    "revision": "dd0e6166b3c8a9532d66291a2d81ec76",
+    "url": "/react-movie-app/static/media/imgNotFound.dd0e6166.png"
+  }
+]);
